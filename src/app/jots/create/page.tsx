61e319bd03eb7bc084a28fdd29cde0f1d5455fb7
Monique <skaiwilliams85@gmail.com>
2023-08-11 12:10:21 -0500

    added button and styled radio inputs

--- src/app/jots/create/page.tsx
+++ src/app/jots/create/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { RefObject, useRef } from "react";
 
 const CreateNote = () => {
@@ -17,9 +18,18 @@ const CreateNote = () => {
   };
 
   return (
-    <form className="w-full max-w-xl mx-auto flex flex-col gap-4 text-[#373736]">
+    <form className="w-full max-w-xl mx-auto flex flex-col gap-4 text-[#373736] relative">
+      <figure className="bg p-2 h-[40px] w-[40px] rounded-[100%] absolute right-0 cursor-pointer hover:bg-black transition-all ease-in-out duration-300">
+        <Image
+          src="/assets/checkmark.svg"
+          alt="checkmark"
+          className="w-full h-full"
+          width={20}
+          height={20}
+        />
+      </figure>
       <textarea
-        className="textarea text-3xl font-bold h-10 focus:ring-0"
+        className="textarea text-3xl font-bold h-10 focus:ring-0 w-[88%]"
         placeholder="title..."
         onInput={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
           handleInput(e, titleRef, 2.5)
@@ -38,17 +48,32 @@ const CreateNote = () => {
 
       <div className="grid grid-cols-3  max-w-xs">
         <span className="flex justify-start items-center gap-1">
-          <input type="radio" name="category" value="general" />
+          <input
+            type="radio"
+            name="category"
+            value="general"
+            className="checked:bg-black checked:hover:bg-black checked:active:bg-black checked:focus:bg-black focus:bg-black focus:outline-none focus:ring-1 focus:ring-black"
+          />
           <label htmlFor="general"> general</label>
         </span>
 
         <span className="flex justify-start items-center gap-1">
-          <input type="radio" name="category" value="todo" />
+          <input
+            type="radio"
+            name="category"
+            value="todo"
+            className="checked:bg-black checked:hover:bg-black checked:active:bg-black checked:focus:bg-black focus:bg-black focus:outline-none focus:ring-1 focus:ring-black"
+          />
           <label htmlFor="todo"> todo</label>
         </span>
 
         <span className="flex justify-start items-center gap-1">
-          <input type="radio" name="category" value="random" />
+          <input
+            type="radio"
+            name="category"
+            value="random"
+            className="checked:bg-black checked:hover:bg-black checked:active:bg-black checked:focus:bg-black focus:bg-black focus:outline-none focus:ring-1 focus:ring-black"
+          />
           <label htmlFor="random"> random</label>
         </span>
       </div>
